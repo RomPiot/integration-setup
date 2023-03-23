aaf796015fda66ddf5dd978323cb8026cf148f81
Romain PIOT <romainpiot.pro@gmail.com>
2023-03-23 23:57:19 +0100

    refacto: server

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ import fs from 'fs';
 import livereload from "livereload";
 import connectLiveReload from "connect-livereload";
 import {createRequire} from "module";
-import path from "path";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 const require = createRequire(import.meta.url);
 const liveReloadServer = livereload.createServer();
-
-liveReloadServer.watch('assets');
-liveReloadServer.watch('templates');
 
 liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
         liveReloadServer.refresh("/");
-    }, 100);
+    }, 10);
 });
 
 const server = express();
@@ -33,27 +27,10 @@ const env = nunjucks.configure('templates', {
     watch: true
 });
 
-env.addGlobal('assets', function (pathUrl) {
-    return pathUrl;
-});
-
-server.set('port', process.env.SERVER_PORT || 3001);
-
 const pageNames = fs.readdirSync('templates').filter(function (file) {
     return !(fs.statSync('templates/' + file).isDirectory() || file === '.' || file === '..');
-
-
 }).map(function (file) {
     return file.replace('.html.twig', '');
-});
-
-// render css & js
-server.get('/build/app.css', function (req, res) {
-    res.sendFile(__dirname + '/public/build/app.css');
-});
-
-server.get('/build/app.js', function (req, res) {
-    res.sendFile(__dirname + '/public/build/app.js');
 });
 
 for (let pageName of pageNames) {
@@ -64,6 +41,14 @@ for (let pageName of pageNames) {
         res.render(pageName + '.html.twig', page_data);
     });
 }
+
+env.addGlobal('assets', function (pathUrl) {
+    return pathUrl;
+});
+
+server.use(express.static('public'));
+
+server.set('port', process.env.SERVER_PORT || 3001);
 
 server.listen(server.get('port'), function () {
     console.log('Server started on port', server.get('port'));
