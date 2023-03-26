313a1208eeafbb89814e0c418fa15fbf0a73ad6a
Romain PIOT <romainpiot.pro@gmail.com>
2023-03-26 14:00:14 +0200

    chore: add default app data to server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectLiveReload from "connect-livereload";
 import {createRequire} from "module";
 import * as dotenv from 'dotenv';
 import {autoloadTwigConfig} from "./twig_config/autoload_twig_config.js";
+import chalk from 'chalk';
 
 dotenv.config();
 
@@ -28,18 +29,32 @@ const env = nunjucks.configure('templates', {
     watch: true
 });
 
-const pageNames = fs.readdirSync('templates').filter(function (file) {
-    return !(fs.statSync('templates/' + file).isDirectory() || file === '.' || file === '..');
-}).map(function (file) {
-    return file.replace('.html.twig', '');
-});
+
+const getPageNames = () => {
+    return fs.readdirSync('templates').filter(function (file) {
+        return !(fs.statSync('templates/' + file).isDirectory() || file === '.' || file === '..');
+    }).map(function (file) {
+        return file.replace('.html.twig', '');
+    });
+}
+
+const pageNames = getPageNames();
+
+const pageToSearch = ['home', 'homepage'];
+for (let pageName of pageToSearch) {
+    if (pageNames.includes(pageName)) {
+        pageNames.splice(pageNames.indexOf(pageName), 1);
+        server.get('/', function (req, res) {
+            const pageData = getPageData(req, pageName);
+            res.render(pageName + '.html.twig', pageData);
+        });
+    }
+}
 
 for (let pageName of pageNames) {
-    console.log('Adding route for page: ' + pageName)
     server.get('/' + pageName, function (req, res) {
-        const page_data = require('./data/' + pageName + '.json');
-
-        res.render(pageName + '.html.twig', page_data);
+        const pageData = getPageData(req, pageName);
+        res.render(pageName + '.html.twig', pageData);
     });
 }
 
@@ -50,5 +65,37 @@ server.use(express.static('public'));
 server.set('port', process.env.SERVER_PORT || 3001);
 
 server.listen(server.get('port'), function () {
-    console.log('Server started on port', server.get('port'));
+    const serverHost = process.env.SERVER_HOST || 'localhost';
+    const serverPort = process.env.SERVER_PORT || 3001;
+    const serverUri = 'http://' + serverHost + ':' + serverPort;
+
+    console.log('Server started on ' + chalk.blueBright(serverUri));
+
+    for (let pageName of pageNames) {
+        console.log('Route enabled: ' + chalk.blueBright(serverUri + '/' + pageName));
+    }
 });
+
+const getPageData = (req, pageName) => {
+    const pageApp = {
+        request: {
+            uri: ''
+        }
+    }
+
+    const pageData = require('./data/' + pageName + '.json');
+    pageData.url = req.protocol + '://' + req.get('host') + '/' + pageName;
+    pageData.app = pageApp;
+    pageData.app.request.uri = req.protocol + '://' + req.get('host') + req.originalUrl;
+    pageData.app.routes = [];
+
+    for (let pageName of getPageNames()) {
+        pageData.app.routes.push({
+            name: pageName,
+            url: req.protocol + '://' + req.get('host') + '/' + pageName
+        });
+    }
+
+    return pageData;
+}
+
